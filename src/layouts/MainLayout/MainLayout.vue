95b65214d204615e1af2b87ee49fcea3a63a5a76
Erkan Özkök <erkanozkok@gmail.com>
2023-04-05 15:26:13 +0300

    bug fix trying

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,31 +1,7 @@
 <script setup lang="ts">
-import { useRoute, useRouter } from "vue-router";
-import Divider from "./Divider.vue";
-import Menu from "./Menu.vue";
-import TopBar from "../../components/TopBar";
-import Alert from "@/base-components/Alert";
-import Lucide from "@/base-components/Lucide";
-import DarkModeSwitcher from "../../components/DarkModeSwitcher";
-import MobileMenu from "../../components/MobileMenu";
-import { useSideMenuStore } from "../../stores/side-menu";
-import { useAlertStore } from "@/stores/alert";
-import { FormattedMenu, nestedMenu, enter, leave } from "./side-menu";
-import Notification from "@/base-components/Notification";
-import Button from "@/base-components/Button";
-import { NotificationElement } from "@/base-components/Notification";
-import {
-  ref,
-  watch,
-  reactive,
-  computed,
-  onMounted,
-  onBeforeMount,
-  provide
-} from "vue";
-import { Dialog } from "@/base-components/Headless";
 
-const route = useRoute()
-const alertStore = useAlertStore()
+import MobileMenu from "../../components/MobileMenu";
+
 </script>
 
 <template>
