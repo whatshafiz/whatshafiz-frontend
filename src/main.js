ad2e8056b553bd416e0e293705637a610a7548b7
Erkan Özkök <erkanozkok@gmail.com>
2023-04-05 11:14:14 +0300

    Sentry integration

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import { createApp } from 'vue'
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import * as Sentry from "@sentry/vue";
 import "./assets/css/app.css";
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_VUE_APP_SENTRY_CRM_FRONTEND_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: ["panel.whatshafiz.com", /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
+app.use(router).use(createPinia()).mount('#app')
