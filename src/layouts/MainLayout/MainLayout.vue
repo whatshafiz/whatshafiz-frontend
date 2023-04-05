00136464cf1ab5830fa4c8862045e11896240106
Erkan Özkök <erkanozkok@gmail.com>
2023-04-05 15:14:04 +0300

    bug fix trying

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -31,9 +31,7 @@ const alertStore = useAlertStore()
 <template>
 <div>
   <div class="py-5 md:py-0">
-    <DarkModeSwitcher />
     <MobileMenu />
-    <TopBar layout="side-menu" />
     <div class="flex overflow-hidden">
       <!-- BEGIN: Side Menu -->
       <nav class="w-[105px] xl:w-[260px] px-5 pb-16 overflow-x-hidden z-50 pt-32 -mt-4 hidden md:block">
@@ -43,38 +41,10 @@ const alertStore = useAlertStore()
           <!-- END: First Child -->
         </ul>
       </nav>
-      <!-- END: Side Menu -->
-      <!-- BEGIN: Content -->
-      <div :class="[
-        'max-w-full md:max-w-none rounded-[30px] md:rounded-none px-4 md:px-[22px] min-w-0 min-h-screen bg-slate-100 flex-1 md:pt-20 pb-10 mt-5 md:mt-1 relative dark:bg-darkmode-700',
-        'before:content-[\'\'] before:w-full before:h-px before:block',
-      ]">
-        <div v-if="alertStore.hasAlertMessage" class="mt-6">
-          <Alert v-if="alertStore.hasSuccessMessage" variant="soft-success" class="items-center mb-2">
-            <div v-for="message in alertStore.getSuccessMessages" class="flex items-center">
-              <Lucide icon="AlertTriangle" class="w-6 h-6 mr-2" />
-              <div class="w-full">{{ message }}</div>
-            </div>
-          </Alert>
 
-          <Alert v-if="alertStore.hasWarningMessage" variant="soft-warning" class="items-center mb-2">
-            <div v-for="message in alertStore.getWarningMessages" class="flex items-center">
-              <Lucide icon="AlertCircle" class="w-6 h-6 mr-2" />
-              <div class="w-full">{{ message }}</div>
-            </div>
-          </Alert>
-
-          <Alert v-if="alertStore.hasErrorMessage" variant="soft-danger" class="items-center mb-2">
-            <div v-for="message in alertStore.getErrorMessages" class="flex items-center">
-              <Lucide icon="AlertOctagon" class="w-6 h-6 mr-2" />
-              <div class="w-full">{{ message }}</div>
-            </div>
-          </Alert>
-        </div>
         <RouterView />
       </div>
       <!-- END: Content -->
-    </div>
   </div>
 
 </div>
