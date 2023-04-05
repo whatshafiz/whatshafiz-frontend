47b08c8f0a14d7ba7c1c884c30cfda99aa3c399b
Erkan Özkök <erkanozkok@gmail.com>
2023-04-05 14:48:31 +0300

    bug fix trying

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -93,10 +93,104 @@ onMounted(() => {
 </script>
 
 <template>
+<div>
   <div class="py-5 md:py-0">
     <DarkModeSwitcher />
     <MobileMenu />
     <TopBar layout="side-menu" />
+    <div class="flex overflow-hidden">
+      <!-- BEGIN: Side Menu -->
+      <nav class="w-[105px] xl:w-[260px] px-5 pb-16 overflow-x-hidden z-50 pt-32 -mt-4 hidden md:block">
+        <ul>
+          <!-- BEGIN: First Child -->
+          <template v-for="(menu, menuKey) in formattedMenu">
+            <Divider v-if="menu == 'divider'" type="li" :class="[
+              'my-6',
+
+              // Animation
+              `opacity-0 animate-[0.4s_ease-in-out_0.1s_intro-divider] animate-fill-mode-forwards`,
+            ]" :key="'divider-' + menuKey"></Divider>
+            <li v-else-if="!menu.ignore && user.can(menu.permission)" :key="menuKey">
+              <Menu :class="{
+                // Animation
+                [`opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards`]: !menu.active,
+              }" :menu="menu" :formattedMenuState="[formattedMenu, setFormattedMenu]" level="first"></Menu>
+              <!-- BEGIN: Second Child -->
+              <Transition @enter="enter" @leave="leave">
+                <ul v-if="menu.subMenu && menu.activeDropdown" :class="[
+                  'bg-white/[0.04] rounded-xl relative dark:bg-transparent',
+                  'before:content-[\'\'] before:block before:inset-0 before:bg-white/30 before:rounded-xl before:absolute before:z-[-1] before:dark:bg-darkmode-900/30',
+                  { block: menu.activeDropdown },
+                  { hidden: !menu.activeDropdown },
+                ]">
+                  <li v-for="(subMenu, subMenuKey) in menu.subMenu" :key="subMenuKey">
+                    <Menu :class="{
+                      // Animation
+                      [`opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards`]: !subMenu.active,
+                    }" :menu="subMenu" :formattedMenuState="[formattedMenu, setFormattedMenu]" level="second"></Menu>
+                    <!-- BEGIN: Third Child -->
+                    <Transition @enter="enter" @leave="leave" v-if="subMenu.subMenu">
+                      <ul v-if="subMenu.subMenu && subMenu.activeDropdown" :class="[
+                        'bg-white/[0.04] rounded-xl relative dark:bg-transparent',
+                        'before:content-[\'\'] before:block before:inset-0 before:bg-white/30 before:rounded-xl before:absolute before:z-[-1] before:dark:bg-darkmode-900/30',
+                        { block: subMenu.activeDropdown },
+                        { hidden: !subMenu.activeDropdown },
+                      ]">
+                        <li v-for="(
+                                  lastSubMenu, lastSubMenuKey
+                                ) in subMenu.subMenu" :key="lastSubMenuKey">
+                          <Menu :class="{
+                            // Animation
+                            [`opacity-0 translate-x-[50px] animate-[0.4s_ease-in-out_0.1s_intro-menu] animate-fill-mode-forwards`]: !lastSubMenu.active,
+                          }" :menu="lastSubMenu" :formattedMenuState="[
+  formattedMenu,
+  setFormattedMenu,
+]" level="third"></Menu>
+                        </li>
+                      </ul>
+                    </Transition>
+                    <!-- END: Third Child -->
+                  </li>
+                </ul>
+              </Transition>
+              <!-- END: Second Child -->
+            </li>
+          </template>
+          <!-- END: First Child -->
+        </ul>
+      </nav>
+      <!-- END: Side Menu -->
+      <!-- BEGIN: Content -->
+      <div :class="[
+        'max-w-full md:max-w-none rounded-[30px] md:rounded-none px-4 md:px-[22px] min-w-0 min-h-screen bg-slate-100 flex-1 md:pt-20 pb-10 mt-5 md:mt-1 relative dark:bg-darkmode-700',
+        'before:content-[\'\'] before:w-full before:h-px before:block',
+      ]">
+        <div v-if="alertStore.hasAlertMessage" class="mt-6">
+          <Alert v-if="alertStore.hasSuccessMessage" variant="soft-success" class="items-center mb-2">
+            <div v-for="message in alertStore.getSuccessMessages" class="flex items-center">
+              <Lucide icon="AlertTriangle" class="w-6 h-6 mr-2" />
+              <div class="w-full">{{ message }}</div>
+            </div>
+          </Alert>
+
+          <Alert v-if="alertStore.hasWarningMessage" variant="soft-warning" class="items-center mb-2">
+            <div v-for="message in alertStore.getWarningMessages" class="flex items-center">
+              <Lucide icon="AlertCircle" class="w-6 h-6 mr-2" />
+              <div class="w-full">{{ message }}</div>
+            </div>
+          </Alert>
+
+          <Alert v-if="alertStore.hasErrorMessage" variant="soft-danger" class="items-center mb-2">
+            <div v-for="message in alertStore.getErrorMessages" class="flex items-center">
+              <Lucide icon="AlertOctagon" class="w-6 h-6 mr-2" />
+              <div class="w-full">{{ message }}</div>
+            </div>
+          </Alert>
+        </div>
+        <RouterView />
+      </div>
+      <!-- END: Content -->
+    </div>
   </div>
   <Notification refKey="successNotification" :options="{ duration: 3000 }" class="flex">
     <Lucide icon="CheckCircle" class="text-success" />
@@ -145,4 +239,5 @@ onMounted(() => {
       </div>
     </Dialog.Panel>
   </Dialog>
+</div>
 </template>
