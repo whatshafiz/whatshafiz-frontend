404ec751cda102f5aaf0f2697314723d048c28aa
Erkan Özkök <erkanozkok@gmail.com>
2023-04-26 17:12:26 +0300

    User role assign

--- src/stores/role.ts
+++ src/stores/role.ts
@@ -13,7 +13,7 @@ export const useRoleStore = defineStore('role', {
       return state.roles
     },
     getIndexURL() {
-      return getBaseUrl('/roles')
+      return getBaseUrl('/roles/paginate')
     }
   },
   actions: {
