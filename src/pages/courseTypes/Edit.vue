492d9adb7a59fabb2576d87e322c3cae20f98690
Erkan Özkök <erkanozkok@gmail.com>
2024-03-22 15:13:30 +0300

    Add course type CRUD operations and UI components

---
+++ src/pages/courseTypes/Edit.vue
@@ -0,0 +1,266 @@
+<script
+  setup
+  lang="ts"
+>
+import { FormLabel, FormInput, FormCheck } from "@/base-components/Form"
+import FormSwitch from '@/base-components/Form/FormSwitch'
+import Button from "@/base-components/Button"
+import LoadingIcon from '@/base-components/LoadingIcon'
+import TomSelect from '@/base-components/TomSelect'
+import { ref, reactive, onBeforeMount, inject, watch } from "vue"
+import { useRouter, useRoute } from "vue-router"
+import { useUserStore } from "@/stores/user"
+import { useCourseTypeStore } from "@/stores/courseType"
+import { useEducationLevelStore } from "@/stores/educationLevel"
+import _ from "lodash";
+
+const successNotificationToggle = inject('successNotificationToggle')
+const isLoading = ref(false)
+const router = useRouter()
+const route = useRoute()
+const user = useUserStore()
+const courseTypeStore = useCourseTypeStore()
+const mainCourseTypes = ref([])
+const educationLevelStore = useEducationLevelStore()
+const educationLevels = ref([])
+const courseTypeId = route.params.courseTypeId
+const courseType = ref({})
+
+onBeforeMount(async () => {
+  mainCourseTypes.value = (await courseTypeStore.fetchCourseTypes()).filter(courseType => courseType.parent_id === null)
+  educationLevels.value = await educationLevelStore.fetchEducationLevels()
+  courseType.value = await courseTypeStore.fetchCourseType(courseTypeId)
+})
+
+watch(() => courseType.name, (newValue) => {
+  courseType.slug = _.camelCase(newValue)
+});
+
+const onSubmit = async () => {
+  isLoading.value = true
+
+  if (await courseTypeStore.updateCourseType(courseTypeId, courseType.value)) {
+    isLoading.value = false
+    successNotificationToggle('İşlem Başarılı', 'Kurs Türü Bilgileri Güncellendi.')
+    router.push({ name: 'courseTypes.index' })
+  } else {
+    isLoading.value = false
+    window.scrollTo(0, 0)
+  }
+}
+</script>
+
+<template>
+  <div v-if="user.can('courseTypes.update')">
+    <div class="flex items-center mt-8 intro-y">
+      <h2 class="mr-auto text-lg font-medium">Kurs Türü Bilgilerini Düzenle</h2>
+    </div>
+    <div class="grid grid-cols-12 gap-6 mt-5">
+      <div class="col-span-12 intro-y lg:col-span-6">
+        <div class="intro-y box">
+          <div class="flex flex-col items-center p-5 border-b sm:flex-row border-slate-200/60 dark:border-darkmode-400">
+            <h2 class="mr-auto text-base font-medium">Kurs Türü Bilgileri</h2>
+          </div>
+          <div class="p-5">
+            <form
+              v-if="courseType.id"
+              class="validate-form"
+              @submit.prevent="onSubmit"
+            >
+              <div
+                class="input-form"
+                v-if="mainCourseTypes.length > 0"
+              >
+                <FormLabel
+                  htmlFor="name"
+                  class="flex flex-col w-full sm:flex-row"
+                >
+                  Ait Olduğu Ana Kurs Türü
+                  <span class="mt-1 text-xs sm:ml-auto sm:mt-0 text-slate-500">
+                    (Ana kurs türü oluşturmak için boş bırakın.)
+                  </span>
+                </FormLabel>
+                <TomSelect
+                  v-model="courseType.parent_id"
+                  :options="{ placeholder: 'Ana Kurs Türü seçin.' }"
+                  class="w-full"
+                >
+                  <option
+                    v-for="mainCourseType in mainCourseTypes"
+                    :key="mainCourseType.id"
+                    :value="mainCourseType.id"
+                  >
+                    {{ mainCourseType.name }}
+                  </option>
+                </TomSelect>
+              </div>
+              <div class="input-form mt-5">
+                <FormLabel
+                  htmlFor="name"
+                  class="flex flex-col w-full sm:flex-row"
+                >
+                  Kurs Türü Adı
+                  <span class="mt-1 text-xs sm:ml-auto sm:mt-0 text-slate-500">
+                    Zorunlu
+                  </span>
+                </FormLabel>
+                <FormInput
+                  id="name"
+                  v-model="courseType.name"
+                  :value="courseType.name"
+                  type="text"
+                  name="name"
+                  required
+                  placeholder="Kurs Türü Adını Yazın"
+                />
+              </div>
+              <div class="input-form mt-5">
+                <FormLabel
+                  htmlFor="slug"
+                  class="flex flex-col w-full sm:flex-row"
+                >
+                  Kurs Türü Slug
+                  <span class="mt-1 text-xs sm:ml-auto sm:mt-0 text-slate-500">
+                    Zorunlu
+                  </span>
+                </FormLabel>
+                <FormInput
+                  id="slug"
+                  v-model="courseType.slug"
+                  :value="courseType.slug"
+                  type="text"
+                  name="slug"
+                  required
+                  placeholder="Kurs Türü Slug Yazın"
+                />
+              </div>
+              <div class="mt-5">
+                <label>Aktif Kurs Türü mü?</label>
+                <div class="mt-2">
+                  <FormSwitch>
+                    <FormSwitch.Input
+                      id="is_active"
+                      type="checkbox"
+                      name="is_active"
+                      v-model="courseType.is_active"
+                    />
+                    <FormSwitch.Label htmlFor="is_active">
+                      Aktif
+                    </FormSwitch.Label>
+                  </FormSwitch>
+                </div>
+              </div>
+              <div class="mt-5">
+                <label>Kurslara Kabul Sınavı Yapılacak mı?</label>
+                <div class="mt-2">
+                  <FormSwitch>
+                    <FormSwitch.Input
+                      id="has_admission_exam"
+                      type="checkbox"
+                      name="has_admission_exam"
+                      v-model="courseType.has_admission_exam"
+                    />
+                    <FormSwitch.Label htmlFor="has_admission_exam">
+                      Evet
+                    </FormSwitch.Label>
+                  </FormSwitch>
+                </div>
+              </div>
+              <div class="input-form mt-5">
+                <FormLabel
+                  htmlFor="name"
+                  class="flex flex-col w-full sm:flex-row"
+                >
+                  Kurslara Katılım İçin En Küçük Yaş Nedir?
+                  <span class="mt-1 text-xs sm:ml-auto sm:mt-0 text-slate-500">
+                    Zorunlu
+                  </span>
+                </FormLabel>
+                <FormInput
+                  id="min_age"
+                  v-model="courseType.min_age"
+                  :value="courseType.min_age"
+                  type="text"
+                  name="min_age"
+                  required
+                  placeholder="Kurs Türü İçin Minimum Yaş Yazın"
+                />
+              </div>
+              <div class="mt-5">
+                <label>Kurslara Katılabilecek Cinsiyetler</label>
+                <div class="flex flex-col mt-2 sm:flex-row">
+                  <FormCheck class="mr-10">
+                    <input
+                      name="genders"
+                      id="genders-male"
+                      type="checkbox"
+                      v-model="courseType.genders"
+                      value="male"
+                    />
+                    <FormCheck.Label htmlFor="genders-male">
+                      Bay
+                    </FormCheck.Label>
+                  </FormCheck>
+                  <FormCheck class="mt-2 mr-2 sm:mt-0">
+                    <input
+                      name="genders"
+                      id="genders-female"
+                      type="checkbox"
+                      v-model="courseType.genders"
+                      value="female"
+                    />
+                    <FormCheck.Label htmlFor="genders-female">
+                      Bayan
+                    </FormCheck.Label>
+                  </FormCheck>
+                </div>
+              </div>
+              <div class="mt-5">
+                <label>Kurslara Katılabilecek Eğitim Seviyeleri</label>
+                <div
+                  v-for="(educationLevel, key) in educationLevels"
+                  :key="key"
+                >
+                  <FormCheck class="mt-3">
+                    <input
+                      :id="'education-level-' + key"
+                      type="checkbox"
+                      name="education_levels"
+                      :value="educationLevel.name"
+                      v-model="courseType.education_levels"
+                    />
+                    <FormCheck.Label :htmlFor="'education-level-' + key">
+                      {{ educationLevel.name }}
+                    </FormCheck.Label>
+                  </FormCheck>
+                </div>
+              </div>
+              <Button
+                variant="primary"
+                type="submit"
+                class="w-1/2 mt-5 mr-2"
+                :disabled="isLoading"
+              >
+                <LoadingIcon
+                  v-show="isLoading"
+                  icon="oval"
+                  color="white"
+                  class="w-4 h-4 mr-5"
+                />
+                Kaydet
+              </Button>
+              <Button
+                variant="outline-secondary"
+                type="button"
+                class="mt-5 mr-5"
+                @click="() => router.go(-1)"
+              >
+                İptal
+              </Button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/pages/courseTypes/Index.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+import Datatable from "@/components/Datatable";
+import Button from "@/base-components/Button";
+import { ref, inject } from "vue";
+import { useRouter } from "vue-router";
+import { stringToHTML } from "@/utils/helper";
+import { useUserStore } from "@/stores/user";
+import { useCourseTypeStore } from "@/stores/courseType";
+import { useAlertStore } from "@/stores/alert";
+
+interface Response {
+  name?: string;
+}
+
+const successNotificationToggle = inject('successNotificationToggle')
+const errorNotificationToggle = inject('errorNotificationToggle')
+
+const tableRef = ref();
+
+const alertStore = useAlertStore()
+const router = useRouter()
+const user = useUserStore()
+const courseTypeStore = useCourseTypeStore()
+
+console.log()
+
+const deleteCourseType = async (courseTypeId) => {
+  if (await courseTypeStore.deleteCourseType(courseTypeId)) {
+    successNotificationToggle('İşlem Başarılı', 'Kurs Türü Silindi!')
+    tableRef.value.refreshData()
+  } else {
+    errorNotificationToggle('HATA', 'Kurs Türü Silinemedi!')
+  }
+}
+
+const tableColumns = [
+  {
+    title: "",
+    formatter: "responsiveCollapse",
+    width: 40,
+    minWidth: 30,
+    hozAlign: "center",
+    resizable: false,
+    headerSort: false,
+  },
+
+  // For HTML table
+  {
+    title: "ID",
+    width: 70,
+    responsive: 0,
+    field: "id",
+    vertAlign: "middle",
+  },
+  {
+    title: "Kurs Türü",
+    minWidth: 200,
+    responsive: 1,
+    field: "name",
+    vertAlign: "middle",
+  },
+  {
+    title: "Kurs Sayısı",
+    minWidth: 150,
+    responsive: 2,
+    field: "courses_count",
+    vertAlign: "middle",
+  },
+  {
+    title: "Whatsapp Grup Sayısı",
+    minWidth: 150,
+    responsive: 3,
+    field: "whatsapp_groups_count",
+    vertAlign: "middle",
+  },
+  {
+    title: "Toplam Başvuru Sayısı",
+    minWidth: 150,
+    responsive: 3,
+    field: "total_users_count",
+    vertAlign: "middle",
+  },
+  {
+    title: "Aktif Kullanıcı Sayısı",
+    minWidth: 150,
+    responsive: 3,
+    field: "active_users_count",
+    vertAlign: "middle",
+  },
+  {
+    title: "Yorum Sayısı",
+    minWidth: 150,
+    responsive: 3,
+    field: "comments_count",
+    vertAlign: "middle",
+  },
+  {
+    title: "İŞLEMLER",
+    width: 150,
+    field: "actions",
+    responsive: 4,
+    hozAlign: "center",
+    headerHozAlign: "center",
+    vertAlign: "middle",
+    headerSort: false,
+    formatter(cell) {
+      const rowData = cell.getData()
+      const buttonsHolder = stringToHTML(`<div class="flex items-center lg:justify-center"></div>`);
+      const showButton =
+        stringToHTML(`<a class="flex items-center mr-3" href="javascript:;">
+                                <i data-lucide="eye" class="w-4 h-4 mr-1"></i> Görüntüle
+                            </a>`);
+      const editButton =
+        stringToHTML(`<a class="flex items-center mr-3" href="javascript:;">
+                                <i data-lucide="check-square" class="w-4 h-4 mr-1"></i> Düzenle
+                            </a>`);
+      const deleteButton =
+        stringToHTML(`<a class="flex items-center text-danger" href="javascript:;">
+                            <i data-lucide="trash-2" class="w-4 h-4 mr-1"></i> Sil
+                            </a>`);
+
+      showButton.addEventListener("click", function () {
+        router.push({ name: 'courseTypes.view', params: { courseTypeId: rowData.id } })
+      });
+      editButton.addEventListener("click", function () {
+        router.push({ name: 'courseTypes.edit', params: { courseTypeId: rowData.id } })
+      });
+      deleteButton.addEventListener("click", function (event) {
+        alertStore.setDeleteModalPreview(true)
+        alertStore.setDeleteModalAction(() => deleteCourseType(rowData.id))
+      });
+
+      buttonsHolder.append(showButton)
+
+      if (user.hasRole('Admin')) {
+        buttonsHolder.append(editButton)
+        buttonsHolder.append(deleteButton)
+      }
+
+      return buttonsHolder
+    },
+  },
+]
+</script>
+
+<template>
+  <div v-if="user.hasRole('Admin')">
+    <div class="flex flex-col items-center mt-8 intro-y sm:flex-row">
+      <h2 class="mr-auto text-lg font-medium">Kurs Türleri</h2>
+      <div class="flex w-full mt-4 sm:w-auto sm:mt-0">
+        <RouterLink :to="{ name: 'courseTypes.create' }">
+          <Button variant="primary" class="mr-2 shadow-md">
+            Yeni Kurs Türü Ekle
+          </Button>
+        </RouterLink>
+      </div>
+    </div>
+
+    <datatable ref="tableRef" :index-url="courseTypeStore.getIndexURL" :columns="tableColumns" />
+  </div>
+</template>
